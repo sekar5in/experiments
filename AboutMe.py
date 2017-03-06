8eade91a16661e9d70f3948288cde79665fd9b11
sekar5in <sekar5inoffice@gmail.com>
2017-03-06 12:48:00 +0530

    Python Project file created

--- AboutMe.py
+++ AboutMe.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 
-import sys
-import os
-import numpy
-
-
 aboutMe = '''My Name is Dhana from TN, India and owning this public repository for my personal scripting knowledge '''
 
 print(aboutMe)
