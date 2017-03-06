f3efa5a979777b3d0a07c835acf4bd12fbb0edc3
sekar5in <sekar5inoffice@gmail.com>
2017-03-06 10:54:30 +0530

    First Commit

---
+++ AboutMe.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import sys
+import os
+import numpy
+
+
+aboutMe = '''My Name is Dhana from TN, India and owning this public repository for my personal scripting knowledge '''
+
+print(aboutMe)
