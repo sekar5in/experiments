888ecb11fe98b2d45e290431948ab1caa78d0d9d
sekar5in <sekar5inoffice@gmail.com>
2017-03-08 21:14:30 +0530

    Python Project file created

---
+++ PrintandStrings.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python3
+
+# This script will just an example for print function and strings function in python 3 interpreter.
+# The above shebang is used explicility to specify the python3.
+
+
+# Python3 print function has to be used always with "()" parenthesis.
+
+print("Hey, My Name is Pandian and do like scripting in python3 and reading books...")
+print('My age is 32 old and currently living in bangalore city.')
+print('Today is 08/03/2017 and little cloudy here...')
+print('Float 5.5 + Integeter 5 ')
+print(float('5.5') + 5)
