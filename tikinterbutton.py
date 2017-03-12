de5725c6d708a216c75dc839d6e8882aa053da82
sekar5in <sekar5inoffice@gmail.com>
2017-03-13 00:02:29 +0530

    Python named function usage

---
+++ tikinterbutton.py
@@ -0,0 +1,29 @@
+#!/usr/local/bin/python3
+
+# This is tkinter to use with button on frame.
+
+from tkinter import *
+
+class Window(Frame):
+
+    def __init__(self, master = None):
+        Frame.__init__(self, master)
+
+        self.master = master
+        self.init_window()
+
+    def init_window(self):
+        self.master.title("GUI")
+        self.pack(fill=BOTH, expand=1)
+        quitButton = Button(self, text ='quit')
+        quitButton.place(x=10, y=10)
+
+
+
+root = Tk()
+
+root.geometry('400x300')
+
+app = Window(root)
+
+root.mainloop()
