e0935df3dd9e9c1592b6a304c052d48d5604840d
sekar5in <sekar5inoffice@gmail.com>
2017-03-16 15:37:33 +0530

    Python named function usage

---
+++ picklemodulebinarydump.py
@@ -0,0 +1,9 @@
+#! /usr/local/bin/python3
+
+# This is the sample program to show the pickle module to dump the strings data into file as binary write and read it.
+
+import pickle
+
+example_Dict = {'2': '4', '4': '5', '9': '20'}
+
+pickle_out =
