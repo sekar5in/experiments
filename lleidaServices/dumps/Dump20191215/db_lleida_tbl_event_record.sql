885ebafb5f4006a0da2718353599a0a1bbdbc057
sekar5in <sekar5in@gmail.com>
2019-12-15 01:45:19 +0530

    lleidaprod ready

--- lleidaServices/dumps/Dump20191215/db_lleida_tbl_event_record.sql
+++ lleidaServices/dumps/Dump20191215/db_lleida_tbl_event_record.sql
@@ -16,38 +16,27 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `tbl_lleida_record`
+-- Table structure for table `tbl_event_record`
 --
 
-DROP TABLE IF EXISTS `tbl_lleida_record`;
+DROP TABLE IF EXISTS `tbl_event_record`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tbl_lleida_record` (
+CREATE TABLE `tbl_event_record` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `tbl_lleida_record_eventid` varchar(255) NOT NULL,
-  `tbl_lleida_record_from` varchar(255) NOT NULL,
-  `tbl_lleida_record_subj` varchar(255) NOT NULL,
-  `tbl_lleida_record_mail_id` varchar(255) NOT NULL,
-  `tbl_lleida_record_msg_id` varchar(255) NOT NULL,
-  `tbl_lleida_record_rcode` varchar(255) NOT NULL,
-  `tbl_lleida_record_reason` varchar(255) NOT NULL,
-  `is_cert_processed` tinyint(1) DEFAULT NULL,
-  `is_addendum_processed` tinyint(1) DEFAULT NULL,
-  `tbl_lleida_record_order_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `CONSTRAINT_1` CHECK (`is_cert_processed` in (0,1)),
-  CONSTRAINT `CONSTRAINT_2` CHECK (`is_addendum_processed` in (0,1))
+  `mail_id` varchar(45) DEFAULT NULL,
+  `mail_date` varchar(45) DEFAULT NULL,
+  `mail_from` varchar(45) DEFAULT NULL,
+  `mail_to` varchar(45) DEFAULT NULL,
+  `mail_subj` varchar(100) DEFAULT NULL,
+  `order_id` varchar(20) DEFAULT NULL,
+  `is_cert_processed` tinyint(4) DEFAULT 0,
+  `is_addendum_processed` tinyint(4) DEFAULT 0,
+  `file_id` int(20) DEFAULT 0,
+  `add_id` int(20) DEFAULT 0,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tbl_lleida_record`
---
-
-LOCK TABLES `tbl_lleida_record` WRITE;
-/*!40000 ALTER TABLE `tbl_lleida_record` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tbl_lleida_record` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -58,4 +47,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-12-02 17:39:35
+-- Dump completed on 2019-12-15  1:44:23
